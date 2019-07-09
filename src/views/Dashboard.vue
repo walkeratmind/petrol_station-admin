e7a82a6a4682d749dd846e13b2a99f9baa4b0316
rakeshniraula <rakeshhniraulaa@gmail.com>
2019-07-09 14:07:56 +0545

    initial commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container dashboard">
+        <h4>Dashboard</h4>
+        <!-- <button class="btn btn-primary">Add Gas Station <i class="fas fa-plus"></i></button> -->
+        <button class="waves-effect waves-light btn">Add Gas Station<i class="material-icons right">add</i></button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
